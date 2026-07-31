70386946126e93f127eedfd3c4bf969b77ea9913
RahulKuVerma <vermarahulkumar307@gmail.com>
2026-07-31 11:10:53 +0530

    Added new folder/files

---
+++ React/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{border:'2px solid violet',width:'1532px',height:'100px',backgroundColor:'lightgreen',position: 'fixed', bottom: 0,left: 0}}>
+ <h4 style={{color:'red',textAlign:'center'}}><u>Address :-
+19th KM Stone, NH-09 Ghaziabad (UP),PIN - 201009<br/>
+Phone no. 01207135112 , Email: info@abes.ac.in</u>  </h4>
+    </div>
+  )
+}
+
+export default Footer
