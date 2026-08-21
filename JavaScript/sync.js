3457ae8590838a5a02edb024453b302a0974cc71
RahulKuVerma <vermarahulkumar307@gmail.com>
2026-08-21 09:28:24 +0530

    sync in js

---
+++ JavaScript/sync.js
@@ -0,0 +1,9 @@
+console.log("task one");
+for(let i=1;i<=100000;i++){
+    console.log(i);
+}
+console.log("task three");
+console.log("task four");
+
+/* In sync Programing  task 1 will execute first and then go to the for loop , 
+it will wait till loop end before the execution of task 3 and at last task 4 will be executed.*/
