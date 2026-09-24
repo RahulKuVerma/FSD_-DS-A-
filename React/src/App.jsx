d2f2265d598294a0f241b93ace4eaa19fffb3ceb
RahulKuVerma <vermarahulkumar307@gmail.com>
2026-09-24 13:50:51 +0530

    React_Route

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import Header from './Components/Header'
-import Main from './Components/Main'
-import Footer from './Components/Footer'
+function Home(){
+ return <h1>This is My Home Page</h1>
+}
+function About(){
+ return <h1>This is My About Page</h1>
+}
 const App = () => {
   return (
     <div>
-  <Header/>
-  <Main/>
-  <Footer/>
+      <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
-
 }
+
 export default App
